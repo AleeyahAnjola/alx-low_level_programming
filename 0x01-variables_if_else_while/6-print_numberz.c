42101082e9cde80218c60cddfc283213a2ee3a29
AleeyahAnjola <anjolaoluwami20@gmail.com>
2022-09-09 15:22:45 -0700

    Task 6: Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num2 = '0';
+
+	for (num2 = '0'; num2 <= '9'; num2++)
+		putchar(num2);
+
+	putchar('\n');
+	return (0);
+}
