3ea273bafe37b581c4f78bb6f1225ef1ac497ca8
AleeyahAnjola <anjolaoluwami20@gmail.com>
2022-09-09 15:01:53 -0700

    Task 4: Alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabetLower = 'a';
+
+	for (alphabetLower = 'a'; alphabetLower <= 'z';)
+	{
+		if (alphabetLower == 'q')
+		{
+			alphabetLower++;
+		}
+		else if (alphabetLower == 'e')
+		{
+			alphabetLower++;
+		}
+		else
+		{
+			putchar(alphabetLower);
+			alphabetLower++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
